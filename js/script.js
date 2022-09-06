b730abe9b713280e9d07da2976d55bf0d30e3eaf
alemonacoo <ale.monaco@outlook.com>
2022-09-06 15:25:42 +0200

    adding cells generator

--- js/script.js
+++ js/script.js
@@ -1,2 +1,15 @@
 console.log("JS OK!");
 
+const cellContainer = document.getElementById("cells-container");
+
+const startButton = document.getElementById("start-button");
+
+startButton.addEventListener('click', function(){
+    console.log('Start Button clicked!');
+    for(let i = 0; i < 100; i++){
+        cellContainer.innerHTML += `<div class="cell">${i+1}</div>`;
+    }
+
+}), {once : true};
+
+
